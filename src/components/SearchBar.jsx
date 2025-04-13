0f5dffd68f19a857b8253989b80c338d528ecbdf
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-04-13 14:12:06 +0300

    made changes to component files to resolve errors when the development server was run

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,15 @@
-// contains search and filter bar for searching through expenses
-import React from 'react' ;
-function SearchBar({ searchTerm, onSearchChange,}) {
-    return (
-        <input
-        type='text'
-        placeholder="Search expenses..."
-        value={searchTerm}
-        onChange={(e) => onSearchChange(e.target.value)}
-        />
-    );
+// filepath: /home/jesse/Development/code/Phase-2/code-challenges/expense-tracker/src/components/SearchBar.jsx
+import React from 'react';
+
+function SearchBar({ searchTerm, onSearchChange }) {
+  return (
+    <input
+      type="text"
+      placeholder="Search expenses..."
+      value={searchTerm}
+      onChange={(e) => onSearchChange(e.target.value)}
+    />
+  );
 }
 
 export default SearchBar;
